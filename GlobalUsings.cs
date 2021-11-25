ab586cc5ebe7af88788acb2534c76fab086f180a
Andy <musictopia2@hotmail.com>
2021-11-25 09:44:43 -0700

    Major Update.

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,5 +1,5 @@
-﻿global using ContactManger.CoreLibrary.ViewModels;
+﻿global using ContactManager.CoreLibrary.ViewModels;
 global using Microsoft.AspNetCore.Components;
 global using System.Diagnostics.CodeAnalysis;
 global using CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.BasicExtensions;
-global using ContactManger.CoreLibrary.Models;
+global using ContactManager.CoreLibrary.Models;
